e636ab083b6eec1abbe49cc16d7e0b3100584be2
Bianca Santana Souza <bsantana@c1r2p2.42sp.org.br>
2023-12-13 15:59:17 -0300

    done

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: bsantana <bsantana@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 15:36:26 by bsantana          #+#    #+#             */
-/*   Updated: 2023/12/12 16:34:36 by bsantana         ###   ########.fr       */
+/*   Updated: 2023/12/13 15:27:17 by bsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@
 # define HEXA_LOWER_BASE "0123456789abcdef"
 
 int		ft_printf(const char *type_format, ...);
-char	ft_flags(char flag, va_list args);
+int		ft_flags(char flag, va_list args);
 int		ft_putchar(char c);
 int		ft_putstr(char *s);
-char	ft_flags(char flag, va_list args);
 int		ft_putnbase(long long n, char *base);
 int		ft_unsigned(long long n, char *base);
 int		ft_strlen(char *str);
+int		ft_pointer(unsigned long long n, char *base);
 
 #endif
--- main.c
+++ main.c
@@ -6,55 +6,55 @@
 /*   By: bsantana <bsantana@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 14:59:49 by bsantana          #+#    #+#             */
-/*   Updated: 2023/12/12 16:49:25 by bsantana         ###   ########.fr       */
+/*   Updated: 2023/12/13 15:39:53 by bsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
+#include <limits.h>
 
 int	main(void)
 {
-// 	ft_printf("Minha função: ");
-// 	ft_printf("%c\n", '0');
-// 	printf("Função original: ");
-// 	printf("%c", '0');
+	ft_printf("Minha função: ");
+	ft_printf("%c\n", '0');
+	printf("Função original: ");
+	printf("%c\n", '0');
 
-	// char *s2 = "Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Atirei o pau no gatis, per gatis num morreus.";
-	// ft_printf("Minha função: ");
-	// ft_printf(" %s %s %s %s %s\n", " - ", "", "4", "", s2);
-	// printf("Função original: ");
-	// printf("%s %s %s %s %s", " - ", "", "4", "", s2);
-
-	// printf("Função original: ");
-	// printf("%d\n", -1);
-	// ft_printf("Meu número: ");
-	// ft_printf("%d\n", -1);
-
-	// printf("Função original: ");
-	// printf("%i\n", 42);
-	// ft_printf("Meu número: ");
-	// ft_printf("%i\n", 42);
-
-	// printf("Função original: ");
-	// printf("%x\n", -10);
-	// ft_printf("Meu hexadecimal minúsculo: ");
-	// ft_printf("%x\n", -10);
-
-	// printf("Função original: ");
-	// printf("%X\n", -10);
-	// ft_printf("Meu hexadecimal maiúsculo: ");
-	// ft_printf("%X\n", -10);
-
-	// printf("Função original: ");
-	// printf("%u\n", -1);
-	// ft_printf("Meu número: ");
-	// ft_printf("%u\n", -1);
+	ft_printf("Minha função: \n");
+	ft_printf("%s\n", "Que bom que você concluiu a ft_prinf!");
+	printf("Função original: \n");
+	printf("%s\n", "Que bom que você concluiu a ft_prinf!");
 
 	printf("Função original: ");
-	printf("%p\n", -10);
+	printf("%d\n", -1);
 	ft_printf("Meu número: ");
-	ft_printf("%p\n", -1);
+	ft_printf("%d\n", -1);
+
+	printf("Função original: ");
+	printf("%i\n", 42);
+	ft_printf("Meu número: ");
+	ft_printf("%i\n", 42);
+
+	printf("Função original: \n");
+	printf("%x\n", -10);
+	ft_printf("Meu hexadecimal minúsculo: \n");
+	ft_printf("%x\n", -10);
+
+	printf("Função original: \n");
+	printf("%X\n", -10);
+	ft_printf("Meu hexadecimal maiúsculo: \n");
+	ft_printf("%X\n", -10);
+
+	printf("Função original: ");
+	printf("%u\n", -1);
+	ft_printf("Meu número: ");
+	ft_printf("%u\n", -1);
+
+	printf("Função original: \n");
+	printf("%p\n", "Você imprime em hexadecimal?");
+	ft_printf("Meu ponteiro: \n");
+	ft_printf("%p\n", "Você imprime em hexadecimal?");
 
 	return (0);
 }
